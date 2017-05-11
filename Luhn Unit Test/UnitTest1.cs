3bbbeaadc36981956b1f495a4d8ea1b9f8948969
carlsargunar <carl@sargunar.com>
2017-05-11 10:14:37 +0100

    Added base proj for Luhn test

---
+++ Luhn Unit Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Luhn_Unit_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
